526ed39eb8390a98035ba932013a8dc027de3269
Timothy McWatters <timmch2o@gmail.com>
2018-01-27 15:48:44 -0600

    Initial commit

---
+++ Face.java
@@ -0,0 +1,19 @@
+/**************
+   Program to: develop a simple Java Program
+               compile and run a Java Program
+               use the Java development enviroment
+               become familiar with basic syntax errors
+   Author: Timothy A McWatters
+   Email Address: tam50@students.uwf.edu
+   Last Changed: 21 May 17
+**************/
+
+public class Face {
+   public static void main(String[] args) {
+      System.out.println("    /////     ");
+      System.out.println("   | o o |    ");
+      System.out.println("  (|  *  |)   ");
+      System.out.println("   | [_] |    ");
+      System.out.println("    -----     ");
+   }
+}
